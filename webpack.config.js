2fa7f5584897ce92666726f684a91309a0dd9342
chttl <chttl@TestD.local>
2016-09-06 15:41:56 +0800

    using plugin: extract-text-webpack-plugin
    https://github.com/webpack/extract-text-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,28 @@
+var extractTextPlugin = require('extract-text-webpack-plugin');
+
 module.exports = {
     entry: {
         bundle: './src/js/app.js'   //js 入口點
     },
     output: {
-        path: './dist/js',
-        filename: '[name].js'   // 動態讀取entry的屬性名稱, 故此輸出為 bundle.js
+        path: './dist',
+        filename: '/js/[name].js'   // 動態讀取entry的屬性名稱, 故此輸出為 bundle.js
     },
     module: {
         loaders: [{
             test: /\.scss$/,    //$表示在字串的最後面    //符合test的描述就會執行底下的loader
-            loader: 'style-loader!css-loader!sass-loader'   //sass->css->style (由右傳至左)
+            //loader: 'style-loader!css-loader!sass-loader'   //sass->css->style (由右傳至左)
+            loader: extractTextPlugin.extract('style-loader','css-loader!sass-loader')
         }, {
             test: /\.js$/,
             loader: 'babel-loader'
         }, {
             test: /\.png$/,
-            loader: 'url-loader?limit=50000&name=../../dist/images/[name].[ext]'  // < 50kb 的圖檔轉成base64格式
+            loader: 'url-loader?limit=50000&name=./images/[name].[ext]'  // < 50kb 的圖檔轉成base64格式
         }]
-    }
+    },
+    plugins: [
+        //new extractTextPlugin('main.css')  //輸出在 dist/js/main.css
+        new extractTextPlugin('./css/main.css')  //輸出在 dist/js/main.css
+    ]
 }
